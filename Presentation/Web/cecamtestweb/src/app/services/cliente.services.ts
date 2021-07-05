3733e4108803081434f62f45087ae23255570e90
Ivan Souza <ivanildo.souza@fahz.com.br>
2021-07-05 12:07:36 -0300

    Correcoa de proxy

--- Presentation/Web/cecamtestweb/src/app/services/cliente.services.ts
+++ Presentation/Web/cecamtestweb/src/app/services/cliente.services.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = '/api';
+const baseUrl = 'http://localhost:14491/api/';
 
 @Injectable({
     providedIn: 'root'
@@ -11,22 +11,22 @@ const baseUrl = '/api';
     constructor(private http: HttpClient) { }
 
      getAll(): Observable<any> {
-        return this.http.get(`${baseUrl}/Cliente/GetAll`);
+        return this.http.get(`${baseUrl}Cliente/GetAll`);
       }
     
       get(id: number): Observable<any> {
-        return this.http.get(`${baseUrl}/Cliente/${id}`);
+        return this.http.get(`${baseUrl}Cliente/${id}`);
       }
     
       create(data: any): Observable<any> {
-        return this.http.post(`${baseUrl}/Cliente/AddCliente`, data);
+        return this.http.post(`${baseUrl}Cliente/AddCliente`, data);
       }
     
       update(id: number, data: any): Observable<any> {
-        return this.http.put(`${baseUrl}/Cliente/UpdateCliente`, data);
+        return this.http.put(`${baseUrl}Cliente/UpdateCliente`, data);
       }
 
       delete(id: number): Observable<any> {
-        return this.http.delete(`${baseUrl}/Cliente/${id}`);
+        return this.http.delete(`${baseUrl}Cliente/${id}`);
       }
   }
